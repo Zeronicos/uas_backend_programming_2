976cd561cbd8bbd53e91916b233be9570ece3bd4
Nico <thomasnico204@gmail.com>
2024-06-28 16:25:11 +0700

    Commit UAS BACKEND PROJECT
    Membuat website untuk managemen produk dimana ada fungsi untuk menampilkan slide, produk, cart, search, filter, dan fungsi custom pada product dimana terdapat pada route productoption bila menekan action button pada dataTable product ('Product Variant')

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Frontend\CartController;
+use App\Http\Controllers\Frontend\DashboardController;
+use App\Http\Controllers\Frontend\FrontendController;
+use App\Http\Controllers\Frontend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +19,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**Admin Auth Routes */
+Route::group(['middleware' => 'guest'], function() {
+    Route::get('admin/login', [AdminAuthController::class, 'index'])->name('admin.login');
+    Route::get('admin/forget-password', [AdminAuthController::class, 'forgetPassword'])->name('admin.forget-password');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('home');
+    Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
+    Route::put('profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
+    Route::post('profile/avatar', [ProfileController::class, 'updateAvatar'])->name('profile.avatar.update');
 });
 
 require __DIR__.'/auth.php';
+
+/** Show Home Page */
+Route::get('/', [FrontendController::class,'index'])->name('home');
+
+/** Show Product Details Page */
+Route::get('/products', [FrontendController::class,'products'])->name('product.index');
+
+/** Show Product Details Page */
+Route::get('/product/{slug}', [FrontendController::class,'showProduct'])->name('product.show');
+
+/** Product Modal Route */
+Route::get('/load-product-modal/{productId}', [FrontendController::class, 'loadProductModal'])->name('load-product-modal');
+
+/** Add To Cart Route */
+Route::post('add-to-cart', [CartController::class, 'addToCart'])->name('add-to-cart');
+Route::get('get-cart-products', [CartController::class, 'getCartProduct'])->name('get-cart-products');
+Route::get('cart-product-remove/{rowId}', [CartController::class, 'cartProductRemove'])->name('cart-product-remove');
+
+/** Cart Page Route */
+Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
+Route::post('/cart-update-qty', [CartController::class, 'cartQtyUpdate'])->name('cart.quantity-update');
+Route::get('/cart-destroy', [CartController::class, 'cartDestroy'])->name('cart.destroy');
+
+/** Coupon Route */
+Route::post('/apply-coupon', [FrontendController::class, 'applyCoupon'])->name('apply-coupon');
+Route::get('/destroy-coupon', [FrontendController::class, 'destroyCoupon'])->name('destroy-coupon');
